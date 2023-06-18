99b9d24dbac83c8e9db4c05544792dd05edce2ad
affahnabilah <affahnabilah31@gmail.com>
2023-06-18 17:26:50 +0700

    first commit

---
+++ database/migrations/2023_06_18_101721_create_karyawan_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('karyawan', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('nik');
+            $table->string('jabatan');
+            $table->enum('jenis_kelamin',['P','L']);
+            $table->string('divisi');
+            $table->string('masa_kerja');
+            $table->string('saldo_cuti');
+            $table->string('gaji');
+            $table->text('alamat');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('karyawan');
+    }
+};
