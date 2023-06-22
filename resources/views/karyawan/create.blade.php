606beaea38cc9f92634f28fb02f5dc9f84586dd9
affahnabilah <affahnabilah31@gmail.com>
2023-06-22 23:28:26 +0700

    create

--- resources/views/karyawan/create.blade.php
+++ resources/views/karyawan/create.blade.php
@@ -1,19 +1,51 @@
+@extends('layouts.master')
+
+@section('content')
+
+<div class="container">
 <h1>create karyawan</h1>
 
 <form action="/karyawan/store" method="POST">
-    @csrf 
-    <input type="text" name="nama" placeholder="nama"><br>
-    <input type="text" name="nik" placeholder="nik"><br>
-    <input type="text" name="jabatan" placeholder="jabatan"><br>
-    <select name="jenis_kelamin">
+  @csrf
+<form>
+<div class="mb-3">
+    <label for="nama" class="form-label">Nama</label>
+    <input type="text" name="nama" class="form-control" id="nama" aria-describedby="emailHelp">
+  </div>
+  
+  <div class="mb-3">
+    <label for="nik" class="form-label">Nik</label>
+    <input type="text" name="nik" class="form-control" id="nik" aria-describedby="emailHelp">
+  </div>
+  <div class="mb-3">
+    <label for="jabatan" class="form-label">Jabatan</label>
+    <input type="text" name="jabatan" class="form-control" id="jabatan" aria-describedby="emailHelp">
+  </div>
+    <select class="form-select" name="jenis_kelamin">
         <option value="">Pilih Jenis Kelamin</option>
         <option value="L">Laki-Laki</option>
         <option value="P">Perempuan</option>
 </select><br>
-<input type="text" name="divisi" placeholder="divisi"><br>
-    <input type="text" name="masa_kerja" placeholder="masa_kerja"><br>
-    <input type="text" name="saldo_cuti" placeholder="saldo_cuti"><br>
-    <input type="text" name="gaji" placeholder="gaji"><br>
-    <textarea name="alamat" id="" cols="30" rows="10"></textarea><br>
-    <input type="submit" name="submit" value="save"><br> 
+<div class="mb-3">
+    <label for="divisi" class="form-label">Divisi</label>
+    <input type="text" name="divisi" class="form-control" id="divisi" aria-describedby="emailHelp">
+  </div>
+  <div class="mb-3">
+    <label for="masa_kerja" class="form-label">Masa Kerja</label>
+    <input type="text" name="masa_kerja" class="form-control" id="masa_kerja" aria-describedby="emailHelp">
+  </div>
+  <div class="mb-3">
+    <label for="saldo_cuti" class="form-label">Saldo Cuti</label>
+    <input type="text" name="saldo_cuti" class="form-control" id="saldo_cuti" aria-describedby="emailHelp">
+  </div>
+  <div class="mb-3">
+    <label for="gaji" class="form-label">Gaji</label>
+    <input type="text" name="gaji" class="form-control" id="gaji" aria-describedby="emailHelp">
+  </div>
+  
+  <label for="alamat" class="form-label">Alamat</label>
+    <textarea class="form-control" name="alamat"></textarea><br>
+    <input type="submit" name="submit" class="btn btn-info" value="save"><br> 
 </form>
+</div>
+@endsection
--- resources/views/karyawan/index.blade.php
+++ resources/views/karyawan/index.blade.php
@@ -1,40 +1,49 @@
-<a href="/karyawan/create">Tambah Data</a>
-<table border ="1"> 
-    <tr>
-        <th>Id</th>
-        <th>Nama</th>
-        <th>Nik</th>
-        <th>Jabatan</th>
-        <td>Jenis Kelamin</th>
-        <th>Divisi</th>
-        <th>Masa Kerja</th>
-        <th>Saldo Cuti</th>
-        <th>Gaji</th>
-        <th>Alamat</th>
-        <th>Aksi</th>
+@extends('layouts.master')
 
-    </tr>
+@section('content')
+    <div class ="container" >
+    <a class="btn btn-primary" href="/karyawan/create">Tambah Data</a>
+         <table class="table">
+            <head class="table-dark">
+            <tr class="table-info">
+                <th>Id</th>
+                <th>Nama</th>
+                <th>Nik</th>
+                <th>Jabatan</th>
+                <th>Jenis Kelamin</th>
+                <th>Divisi</th>
+                <th>Masa Kerja</th>
+                <th>Saldo Cuti</th>
+                <th>Gaji</th>
+                <th>Alamat</th>
+                <th>Aksi</th>
+
+            </tr>
      @foreach($karyawan as $k)
-    <tr>
+        <tr>
 
-         <td>{{$k->id}}</td>
-        <td>{{$k->nama}}</td>
-        <td>{{$k->nik}}</td>
-        <td>{{$k->jabatan}}</td>
-        <td>{{$k->jenis_kelamin}}</td>
-        <td>{{$k->divisi}}</td>
-        <td>{{$k->masa_kerja}}</td>
-        <td>{{$k->saldo_cuti}}</td>
-        <td>{{$k->gaji}}</td>
-        <td>{{$k->alamat}}</td>
+                <td>{{$k->id}}</td>
+                <td>{{$k->nama}}</td>
+                <td>{{$k->nik}}</td>
+                <td>{{$k->jabatan}}</td>
+                <td>{{$k->jenis_kelamin}}</td>
+                <td>{{$k->divisi}}</td>
+                <td>{{$k->masa_kerja}}</td>
+                <td>{{$k->saldo_cuti}}</td>
+                <td>{{$k->gaji}}</td>
+                <td>{{$k->alamat}}</td>
         <td>
-        <a href="/karyawan/{{$k->id}}/edit">Edit</a>
-        <form action="/karyawan/{{$k->id}}" method="POST">
-            @csrf
-            @method('delete')
-            <input type="submit" value="Delete">
-        </form>
-    </td>
-</tr>
-@endforeach
-</table> 
+            <div class="btn-group" role="group" aria-label="Basic example">
+                 <a class="btn btn-warning" href="/karyawan/{{$k->id}}/edit">Edit</a>
+                 <form action="/karyawan/{{$k->id}}" method="POST">
+                   @csrf
+                   @method('delete')
+                  <input class="btn btn-danger" type="submit" value="Delete" onclick="confirm('sure ?')">
+                  </form>
+            </div>
+        </td>
+        </tr>
+    @endforeach
+    </table> 
+    </div>
+@endsection
